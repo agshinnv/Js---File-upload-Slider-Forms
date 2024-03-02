05eed804056acf7df42165ce6c8394215857b19d
Agshin N. Valiyev <agshinnv@code.local>
2024-03-02 14:52:07 +0400

    task added

---
+++ SliderTask/assets/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+
+
+
+let leftIcon = document.querySelector(".slider .icons .left-icon");
+let rightIcon = document.querySelector(".slider .icons .right-icon");
+
+
+function rightSlider(){
+    let activeImage = document.querySelector(".active-image");
+    activeImage.classList.remove("active-image");
+    if(activeImage.nextElementSibling != null){
+        activeImage.nextElementSibling.classList.add("active-image");
+    }else{
+        rightIcon.parentNode.nextElementSibling.firstElementChild.classList.add("active-image");
+    }
+}
+
+
+function leftSlider(){
+    let activeImage = document.querySelector(".active-image");
+    activeImage.classList.remove("active-image");
+    if(activeImage.previousElementSibling != null){
+        activeImage.previousElementSibling.classList.add("active-image");
+    }else{
+        leftIcon.parentNode.nextElementSibling.lastElementChild.classList.add("active-image");
+    }
+}
+
+rightIcon.addEventListener("click",rightSlider);
+leftIcon.addEventListener("click",leftSlider);
